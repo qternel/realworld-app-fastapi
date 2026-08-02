caa525e43f2bda561c380d7b7735dfeb8c3c6dcd
Michael Sidorchenko <qternel@gmail.com>
2026-08-02 18:02:54 +0300

    make error message structure correspond to the one in specification

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from api import articles, auth, profiles, user
 from db.database import Base, engine
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+
+@app.exception_handler(HTTPException)
+async def custom_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code, content={"errors": {"body": [exc.detail]}}
+    )
+
 
 Base.metadata.create_all(engine)
 
